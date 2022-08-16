56494018feca3eb677bfe8c3dc8099550ce703c5
Victor Micheal <michealvictor175@gmail.com>
2022-08-16 09:13:23 +0100

    Add project files.

---
+++ DelegateClassWk/Delegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classworkDel
+{
+
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public class Delegate
+    {
+            //predicate
+        public delegate bool Predicate(XClass x); 
+        
+          //method that takes in the delegate
+        public bool WhereOne(Predicate y)       
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            var repoUser = new Delegate();
+            var t = new XClass();
+            var likelyUsername = t.FirstName + "." + t.LastName;
+            var dUserCount = repoUser.WhereOne(x => x.UserName == likelyUsername);
+        }
+
+    }
+    /// <summary>
+    /// Public class with the properties
+    /// </summary>
+    public class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string UserName { get; set; }
+    }
+}
